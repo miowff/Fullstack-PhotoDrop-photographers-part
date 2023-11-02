c83c892673aa4df7b009bc865920a32c27771b26
Mykola <95630245+miowff@users.noreply.github.com>
2023-11-02 14:42:26 +0200

    vite config fixes

--- forontend/vite.config.ts
+++ forontend/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import pluginRewriteAll from "vite-plugin-rewrite-all";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), pluginRewriteAll()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
