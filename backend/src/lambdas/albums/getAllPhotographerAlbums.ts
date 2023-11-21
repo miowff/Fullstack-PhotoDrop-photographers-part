8a661671df2699e6384e1ad5cf6841d3f9f40a0c
Mykola <95630245+miowff@users.noreply.github.com>
2023-11-21 18:36:03 +0200

    validation

--- backend/src/lambdas/albums/getAllPhotographerAlbums.ts
+++ backend/src/lambdas/albums/getAllPhotographerAlbums.ts
@@ -1,17 +1,18 @@
+import middy from "@middy/core";
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
 } from "aws-lambda/trigger/api-gateway-proxy";
+import { errorHandlerMiddleware } from "src/middleware/errorHandler";
 import { albumsService } from "src/services/albumsService";
 import responseCreator from "src/services/utils/responseCreator";
-export const handler = async (
+const getAllPhotographerAlbums = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  try {
-    const { photographerId } = event.requestContext.authorizer?.lambda;
-    const albums = await albumsService.getAllPhotographerAlbums(photographerId);
-    return responseCreator.default(JSON.stringify(albums), 200);
-  } catch (err) {
-    return responseCreator.error(err);
-  }
+  const { photographerId } = event.requestContext.authorizer?.lambda;
+  const albums = await albumsService.getAllPhotographerAlbums(photographerId);
+  return responseCreator.default(JSON.stringify(albums), 200);
 };
+export const handler = middy()
+  .use(errorHandlerMiddleware())
+  .handler(getAllPhotographerAlbums);
--- backend/src/lambdas/users/getAvailableNumbers.ts
+++ backend/src/lambdas/users/getAvailableNumbers.ts
@@ -1,13 +1,13 @@
+import middy from "@middy/core";
 import { APIGatewayProxyResult } from "aws-lambda/trigger/api-gateway-proxy";
+import { errorHandlerMiddleware } from "src/middleware/errorHandler";
 import { usersService } from "src/services/usersService";
-
 import responseCreator from "src/services/utils/responseCreator";
 
-export const handler = async (): Promise<APIGatewayProxyResult> => {
-  try {
-    const availableNumbers = await usersService.getAllAvailableNumbers();
-    return responseCreator.default(JSON.stringify(availableNumbers), 200);
-  } catch (err) {
-    return responseCreator.error(err);
-  }
+const getAllAvailableNumbers = async (): Promise<APIGatewayProxyResult> => {
+  const availableNumbers = await usersService.getAllAvailableNumbers();
+  return responseCreator.default(JSON.stringify(availableNumbers), 200);
 };
+export const handler = middy()
+  .use(errorHandlerMiddleware())
+  .handler(getAllAvailableNumbers);
